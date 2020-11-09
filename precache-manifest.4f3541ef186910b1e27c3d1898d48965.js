b24ce2f7efff6b064df81d6f8ffc882bd4e7fcee
Bacancy <khushbu.kosto@bacancytechnology.com>
2020-11-09 14:07:06 +0530

    Updates

---
+++ precache-manifest.4f3541ef186910b1e27c3d1898d48965.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1caf0789ee8fdb1f0b3c",
+    "url": "/me/static/js/runtime~main.1caf0789.js"
+  },
+  {
+    "revision": "c162d51f31313002aaa8",
+    "url": "/me/static/js/main.c162d51f.chunk.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/me/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "c162d51f31313002aaa8",
+    "url": "/me/static/css/main.84aa5f41.chunk.css"
+  },
+  {
+    "revision": "e8ddf823c2934cae4e111830c5f04321",
+    "url": "/me/index.html"
+  }
+];
